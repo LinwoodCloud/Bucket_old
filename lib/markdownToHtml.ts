fb964c5667ad7696b34d91a1e6315201c242b2a4
CodeDoctorDE <janandrejdiehl@gmail.com>
2021-09-05 09:12:26 +0200

    add node_modules

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -2,6 +2,8 @@ import {remark} from 'remark'
 import remarkHtml from 'remark-html'
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(remarkHtml).process(markdown)
-  return result.toString()
+  return remark()
+    .use(remarkHtml)
+    .process(markdown)
+    .then((contents) => contents.toString());
 }
--- pages/docs/index.tsx
+++ pages/docs/index.tsx
@@ -1,11 +1,14 @@
 import { getDocPage, getDocs } from '../../lib/docs';
+import markdownToHtml from '../../lib/markdownToHtml';
 import DocsPage from './[slug]';
 
 export default DocsPage;
 export async function getStaticProps({ params }) {
+  var page = getDocPage('');
+  page.content = await markdownToHtml(page.content);
   return {
       props: {
-          page: getDocPage(''),
+          page: page,
           pages: getDocs()
       },
   };
