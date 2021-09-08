260f9f4449b2311bd5452678a5482d66a2c7488b
CodeDoctor <wusel905@mail.com>
2021-09-08 19:53:35 +0200

    add icon buttons to assets

--- pages/assets/[collection]/[item].tsx
+++ pages/assets/[collection]/[item].tsx
@@ -1,7 +1,8 @@
 import { ChevronDownIcon } from '@chakra-ui/icons'
 import { Input } from '@chakra-ui/input'
 import { Box, Container, Flex, Heading, Stack, VStack } from '@chakra-ui/layout'
-import { Button, Menu, MenuButton, MenuDivider, MenuItem, MenuList, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react'
+import { Button, ButtonGroup, IconButton, Menu, MenuButton, MenuDivider, MenuItem, MenuList, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react'
+import { ArrowSquareOut, Bell } from 'phosphor-react'
 import React, { ReactElement } from 'react'
 import Card from '../../../components/card'
 import BucketNavigation from '../../../components/nav'
@@ -15,17 +16,20 @@ export default function AssetsPage(): ReactElement {
                     <Heading size="xl" p={4} flex="1">
                         Asset name
                     </Heading>
-                    <Menu placement="bottom-end">
-                        <MenuButton colorScheme="blue" as={Button} rightIcon={<ChevronDownIcon />}>
-                            Get
-                        </MenuButton>
-                        <MenuList>
-                            <MenuItem>Subscribe</MenuItem>
-                            <MenuDivider />
-                            <MenuItem>Download</MenuItem>
-                            <MenuItem>Visit website</MenuItem>
-                        </MenuList>
-                    </Menu>
+                    <ButtonGroup>
+                        <IconButton aria-label="Subscribe" icon={<Bell size={24} />} />
+                        <IconButton aria-label="Visit website" icon={<ArrowSquareOut size={24} />} />
+                        <Menu placement="bottom-end">
+                            <MenuButton colorScheme="blue" as={Button} rightIcon={<ChevronDownIcon />}>
+                                Get
+                            </MenuButton>
+                            <MenuList>
+                                <MenuItem>Stable</MenuItem>
+                                <MenuItem>Beta</MenuItem>
+                                <MenuItem>Alpha</MenuItem>
+                            </MenuList>
+                        </Menu>
+                    </ButtonGroup>
                 </Flex>
                 <Tabs>
                     <TabList>
